8bad78d7e40b3e6b43a404ca7009cf4d42e3b952
Windoze <windoze.net@gmail.com>
2014-02-20 22:48:23 +0800

    Initial Commit

---
+++ fiberserver/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  fiberserver
+//
+//  Created by Chen Xu on 14-2-20.
+//  Copyright (c) 2014年 0d0a.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
